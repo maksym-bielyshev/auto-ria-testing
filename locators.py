5b70deb5d78e67535d4b98cfb13596a07fbcafa4
Maksym Bielyshev <maksym.bielyshev@gmail.com>
2020-12-18 12:35:59 +0200

    Locators changed

--- locators.py
+++ locators.py
@@ -4,60 +4,43 @@ from selenium.webdriver.common.by import By
 class LocatorsHomeFilter:
     ALL_AUTOS = (
         By.XPATH,
-        "//body/div[@id='app']/main[1]/div[2]/div[2]/form[1]/div[1]/label[1]"
-    )
+        "//input[@id='allRadioType']")
     USED_AUTOS = (
         By.XPATH,
-        "//body/div[@id='app']/main[1]/div[2]/div[2]/form[1]/div[1]/label[2]"
-    )
+        "//input[@id='buRadioType']")
     NEW_AUTOS = (
         By.XPATH,
-        "//body/div[@id='app']/main[1]/div[2]/div[2]/form[1]/div[1]/label[3]"
-    )
+        "//input[@id='naRadioType']")
     AUTOS_FOR_DELIVERY = (
         By.XPATH,
-        "//body/div[@id='app']/main[1]/div[2]/div[2]/form[1]/div[1]/label[4]"
-    )
-    CATEGORY_DROPDOWN = (
-        By.XPATH,
-        "//select[@id='categories']"
-    )
-    BRAND_DROPDOWN = (
-        By.XPATH,
-        "//input[@id='brandTooltipBrandAutocompleteInput-brand']"
-    )
-    MODEL_DROPDOWN = (
-        By.XPATH,
-        "//body/div[@id='app']/main[1]/div[2]/div[2]/form[1]/div[2]/div[1]/"
-        "div[3]/div[1]/div[2]/div[1]/label[1]"
-    )
-    REGION_DROPDOWN = (
-        By.XPATH,
-        "//input[@id='brandTooltipBrandAutocompleteInput-region']"
-    )
-    YEAR_FROM_DROPDOWN = (
-        By.XPATH,
-        "//select[@id='yearFrom']"
-    )
-    YEAR_TO_DROPDOWN = (
-        By.XPATH,
-        "//select[@id='yearTo']"
-    )
-    PRICE_FROM = (
-        By.XPATH,
-        "//input[@id='priceFrom']"
-    )
-    PRICE_TO = (
-        By.XPATH,
-        "//input[@id='priceTo']"
-    )
-    SEARCH_BUTTON = (
-        By.XPATH,
-        "//body/div[@id='app']/main[1]/div[2]/div[2]/form[1]/div[3]/button[1]/"
-        "span[1]"
-    )
+        "//input[@id='orderRadioType']")
     CHECKED_VIN = (
         By.XPATH,
-        "//body/div[@id='app']/main[1]/div[2]/div[2]/form[1]/div[2]/div[3]/"
-        "label[1]/span[1]"
-    )
+        "//input[@id='verifiedVIN']")
+    CATEGORY_DROPDOWN = (
+        By.XPATH,
+        "//select[@id='categories']")
+    BRAND_DROPDOWN = (
+        By.XPATH,
+        "//input[@id='brandTooltipBrandAutocompleteInput-brand']")
+    MODEL_DROPDOWN = (
+        By.XPATH,
+        "//input[@id='brandTooltipBrandAutocompleteInput-model']")
+    REGION_DROPDOWN = (
+        By.XPATH,
+        "//input[@id='brandTooltipBrandAutocompleteInput-region']")
+    YEAR_FROM_DROPDOWN = (
+        By.XPATH,
+        "//select[@id='yearFrom']")
+    YEAR_TO_DROPDOWN = (
+        By.XPATH,
+        "//select[@id='yearTo']")
+    PRICE_FROM = (
+        By.XPATH,
+        "//input[@id='priceFrom']")
+    PRICE_TO = (
+        By.XPATH,
+        "//input[@id='priceTo']")
+    SEARCH_BUTTON = (
+        By.XPATH,
+        "//button[@type='submit']//span[contains(text(),'Пошук')]")
