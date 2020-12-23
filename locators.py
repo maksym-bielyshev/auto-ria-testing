8a775e3ab2065205eb3338088d9bfada3390728e
Maksym Bielyshev <maksym.bielyshev@gmail.com>
2020-12-23 11:33:24 +0200

    Refactor a locator

--- locators.py
+++ locators.py
@@ -36,4 +36,4 @@ class LocatorsHomeFilter:
     PRICE_TO = (
         By.XPATH, "//input[@id='priceTo']")
     SEARCH_BUTTON = (
-        By.CSS_SELECTOR, "button[type='submit']")
+        By.XPATH, "//button[@type='submit']//span")
