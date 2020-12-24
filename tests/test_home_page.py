0ce828717d59e30d834fc05c956b55eadec7749d
Maksym Bielyshev <maksym.bielyshev@gmail.com>
2020-12-24 11:02:00 +0200

    Clarify the documentation

--- tests/test_home_page.py
+++ tests/test_home_page.py
@@ -24,11 +24,14 @@ class TestHomePage(BaseTest):
         get_test_data_dictreader('category_dropdown.csv'))
     def test_categories_dropdown_transition(
             self, language, category, substring) -> None:
-        """Check all the options in the "Categories" dropdown.
+        """Check the options in the "Categories" dropdown where each option has
+        a specific naming for categories in the address.
+
         Steps:
             1. Click on the "Categories" dropdown
             2. Select an option
             3. Click on the "Пошук" button
+
         Expected result:
             1. Link in the address bar is changed to the address of the
                corresponding category.
@@ -45,13 +48,14 @@ class TestHomePage(BaseTest):
         get_test_data('category_dropdown_without_naming.csv'))
     def test_categories_dropdown_transition_without_naming(
             self, language, category, substring) -> None:
-        """Check options in the "Categories" dropdown, which have no
-        specific naming for categories in the address.
+        """Check the options in the "Categories" dropdown where each option has
+        no specific naming for categories in the address.
 
         Steps:
             1. Click on the "Categories" dropdown
             2. Select an option
             3. Click on the "Пошук" button
+
         Expected result:
             1. Link in the address bar is changed to the address of the
                corresponding category.
