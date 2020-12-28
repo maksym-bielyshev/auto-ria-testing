22ffe2d728c468dd413281da930201169e1142d3
Maksym Bielyshev <maksym.bielyshev@gmail.com>
2020-12-28 02:20:23 +0200

    Remove long lines

--- pages/base_page.py
+++ pages/base_page.py
@@ -15,12 +15,14 @@ class BasePage:
 
     def switch_proper_language(self, language):
         if language == "ua" and "/uk/" not in self._driver.current_url:
-            ukrainian_link = WebDriverWait(
-                self._driver, 10).until(EC.visibility_of_element_located(
-                LocatorsBasePage.UKRAINIAN_LINK))
+            ukrainian_link = \
+                WebDriverWait(self._driver, 10).until(
+                    EC.visibility_of_element_located(
+                        LocatorsBasePage.UKRAINIAN_LINK))
             ukrainian_link.click()
         elif language == "ru" and "/uk/" in self._driver.current_url:
-            russian_link = WebDriverWait(
-                self._driver, 10).until(EC.visibility_of_element_located(
-                LocatorsBasePage.RUSSIAN_LINK))
+            russian_link = \
+                WebDriverWait(self._driver, 10).until(
+                    EC.visibility_of_element_located(
+                        LocatorsBasePage.RUSSIAN_LINK))
             russian_link.click()
